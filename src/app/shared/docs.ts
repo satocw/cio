cb24941e8665baf36aa5e5235c8bc406678a725d
NISHI Satoshi <nishi@flexsche.com>
2019-01-24 18:59:01 +0900

    doc link

--- src/app/shared/docs.ts
+++ src/app/shared/docs.ts
@@ -1,10 +1,19 @@
 import { DocumentContents } from 'app/documents/document-contents';
 import index from '_doc/index';
 import server_setup from '_doc/server/setup';
+import server_pages_config from '_doc/server/pages/config';
+import server_api_reschedule from '_doc/server/api/reschedule';
 
 export const DOCUMENTS: {
   [id: string]: DocumentContents;
 } = {
+  // トップページ
   index,
-  'server/setup': server_setup
+
+  // サーバー
+  'server/setup': server_setup,
+  'server/pages/config': server_pages_config,
+  'server/api/reschedule': server_api_reschedule
+
+  // クライアント
 };
