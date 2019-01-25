ba80fc1ea196a6f3ec684179cc5bff7595f9312a
NISHI Satoshi <nishi@flexsche.com>
2019-01-25 11:14:12 +0900

    search

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -26,9 +26,28 @@ export class SearchService {
         .map(id => {
           const doc = DOCUMENTS[id];
           const contents = doc.contents;
-          return contents.includes(query)
-            ? { path: id, title: doc.title }
-            : null;
+          const idx = contents.toLowerCase().indexOf(query.toLowerCase()); // 小文字大文字を区別せずに検索
+          if (idx > -1) {
+            const startTagEnd = contents.lastIndexOf('>', idx);
+            const startTagStart = contents.lastIndexOf('<', idx);
+            const endTagStart = contents.indexOf('<', idx);
+            const endTagEnd = contents.indexOf('>', idx);
+
+            if (startTagStart > startTagEnd || endTagStart > endTagEnd) {
+              // タグ内の文字列にひっかかった
+              return null;
+            }
+
+            return {
+              path: id,
+              title: doc.title,
+              keywords: contents.slice(startTagEnd + 1, endTagStart) // 見つかった文字列の直前の「>」から直後の「<」までを切り出す
+              // idx: idx,  // For Debug
+              // start: startTagIdx,  // For Debug
+              // end: endTagIdx // For Debug
+            };
+          }
+          return null;
         })
         .filter(val => !!val);
     }
